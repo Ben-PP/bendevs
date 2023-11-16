582ac0005e8c12f1c47e18a949a871cf8c9196fb
Karel Parkkola <karel.parkkola@gmail.com>
2023-11-16 13:11:46 +0200

    Added sanity tokens to env

--- packages/web-next/src/sanityClient.js
+++ packages/web-next/src/sanityClient.js
@@ -1,8 +1,10 @@
+import 'dotenv/config'
 import { createClient } from 'next-sanity'
 const sanityClient = createClient({
   projectId: 'c1ml1moo',
   dataset: 'production',
-  useCdn: false
+  useCdn: false,
+  token: process.env.SANITY_READ_TOKEN
 })
 
 export default sanityClient
